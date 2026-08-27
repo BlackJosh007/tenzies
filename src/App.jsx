6b95fd7dfb64bd0d73f36ec98e6add5177d00db6
Joshua ADEGOKE <skento007@gmail.com>
2026-08-27 15:02:59 +0100

    Implemented some functionality to the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
 import Die from "./component/Die.jsx"
 import "./App.css"
 import { use, useState } from "react"
+import { nanoid, random } from 'nanoid'
 
 export default function App() {
     function generateAllNewDice() {
-        let randomArray = Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1)
-
+        const randomArray = Array.from({ length: 10 }, () => (
+            {
+                value: Math.floor(Math.random() * 6) + 1,
+                isHeld: false,
+                id: nanoid()
+            }
+        ))
+        /* console.log(randomArray[0]) */
         return randomArray
     }
+    /* generateAllNewDice() */
 
-    generateAllNewDice()
-
-    const [diceNo, setDiceNo] = useState(generateAllNewDice)
+    const [diceNo, setDiceNo] = useState(generateAllNewDice())
 
     function rollDice() {
-        setDiceNo(generateAllNewDice)
+        setDiceNo(prevDice => prevDice.map(
+            dice => dice.isHeld == false ?
+                { ...dice, value: Math.floor(Math.random() * 6) + 1 }
+                : dice
+        ))
+    }
+
+
+    function hold(id) {
+        setDiceNo(prevDice => prevDice.map(
+            dice => dice.id === id ?
+                { ...dice, isHeld: !dice.isHeld }
+                : dice
+        )
+        )
     }
 
     return (
         <main>
             <div className="dice-container">
-                {diceNo.map(dice => <Die
-                    value={dice} />)}
+                {diceNo.map(diceObj => <Die
+                    key={diceObj.id}
+                    value={diceObj.value}
+                    isHeld={diceObj.isHeld}
+                    holdFunc={() => hold(diceObj.id)}
+                    uniqueKey={diceObj.id} />)}
             </div>
 
             <button onClick={rollDice} className="roll-dice">Roll</button>
--- src/component/Die.jsx
+++ src/component/Die.jsx
@@ -1,6 +1,12 @@
 import "./Die.css"
-export default function Die({ value }) {
+export default function Die({ value, isHeld, holdFunc, uniqueKey }) {
+    const style = {
+        background: isHeld ? "#59E391" : "white"
+    }
     return (
-        <button className="dice-btn">{value}</button>
+        <button style={style}
+            className="dice-btn"
+            onClick={holdFunc}>{value}
+        </button>
     )
 }
